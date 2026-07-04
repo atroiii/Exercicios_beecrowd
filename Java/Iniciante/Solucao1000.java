07fa9320203338c1bf482186aad295582066928d
Arthur Felipe <arthurtroiii513@gmail.com>
2026-07-03 23:35:22 -0300

    feat(iniciante): add soluções exercícios 1000 ao 1003 realizando em Python

--- Java/Iniciante/Solucao1000.java
+++ Java/Iniciante/Solucao1000.java
@@ -2,7 +2,7 @@ package Iniciante;
 
 import java.io.IOException;
 
-public class solucao {
+public class Solucao_1000 {
     public static void main(String [] args) throws IOException {
         System.out.println("Hello World!");
     }
